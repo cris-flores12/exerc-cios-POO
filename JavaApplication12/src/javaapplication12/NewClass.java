6f4a305f8ef917ced46c17a4925722d0110d3f26
cris-flores12 <crisfloresrs@gmail.com>
2026-02-26 14:50:40 -0300

    ex1 finalizado

---
+++ JavaApplication12/src/javaapplication12/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author crisf
+ */
+public class NewClass {
+    String nome;
+int idade;
+public void apresentar(){
+    System.out.println("meu nome é " + nome);
+    System.out.println("eu tenho " + idade+ " anos de idade");
+   
+}
+}
+
+
